4d7621bad46b9916d375382e104c50c8306d8f5b
Zainmir <zainnaeemk10@gmail.com>
2021-01-28 09:34:30 +0000

    starting changes with reat styl

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const address ="https://picsum.photos/200?grayscale"
+
 const customestyle={
-  color:"red",
-  fontSize:"20px",
-  borderColor:"black"
+  color:"",
 }
-customestyle.color="green"
+const date = new Date();
+const current =date.getHours();
+
+let greeting;
+if(current<12){
+    greeting="Good morning"
+    customestyle.color="red"
+}else if(current<18){
+  greeting="goodafternoon"
+  customestyle.color="Green"
+}else{
+  greeting="Good night"
+  customestyle.color="blue"
+}
+
 ReactDOM.render(
-  <div>
-  <h1 style={customestyle}>My heading</h1>
-   
-  </div>,
+ <h1 style={customestyle}>{greeting}</h1>,
   document.getElementById('root')
 );
 //its working

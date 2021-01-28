c61846ffba26a48755f3fe3395aa21c0c4b36cad
Zainmir <zainnaeemk10@gmail.com>
2021-01-28 19:00:25 +0000

    breaking in components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Heading from "./heading";
+import {add,multiply,subtract,divide} from "./methods"
 import List from "./list";
 
 function App() {
     return (<div>
-    <Heading />
+
+    <ul>
+      <li>
+        {add(1,2)}
+      </li>
+      <li>
+        {multiply(2,3)}
+      </li>
+      <li>
+        {subtract(3,4)}
+      </li>
+      <li>
+        {divide(6,6)}
+      </li>
+
+      
+    </ul>
     
   </div>
     );

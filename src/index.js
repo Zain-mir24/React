b8964b77fe6d0d672cd888ace02901462b228e78
Zainmir <zainnaeemk10@gmail.com>
2021-01-28 10:02:00 +0000

    components in react

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"
 
-const address ="https://picsum.photos/200?grayscale"
-
-const customestyle={
-  color:"",
-}
-const date = new Date();
-const current =date.getHours();
-
-let greeting;
-if(current<12){
-    greeting="Good morning"
-    customestyle.color="red"
-}else if(current<18){
-  greeting="goodafternoon"
-  customestyle.color="Green"
-}else{
-  greeting="Good night"
-  customestyle.color="blue"
-}
 
 ReactDOM.render(
- <h1 style={customestyle}>{greeting}</h1>,
-  document.getElementById('root')
+  App(),
+  document.getElementById("root")
 );
 //its working
-
---
+++ src/list.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+function List(){
+   return (<ul>
+        <li>
+            zain
+        </li>
+        <li>
+            ahmed
+        </li>
+        <li>
+            mir
+        </li>
+    </ul>
+    );
+}
+export default List;

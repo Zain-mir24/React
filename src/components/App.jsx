7687730d4cded42bf5da3c2d1d3345a19119a75b
Zainmir <zainnaeemk10@gmail.com>
2021-01-29 16:06:52 +0000

    keeperapp-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,1 @@
 import React from 'react';
-import Heading from "./heading";
-import {add,multiply,subtract,divide} from "./methods"
-import List from "./list";
-
-function App() {
-    return (
-    <Heading />
-    );
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App"
-import {add,multiply,subtract,divide} from "./components/methods"
+//1. Create a new React app.
+//2. Create a App.jsx component.
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://l1pp6.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App'
 
 ReactDOM.render(
-   <App />,
-  document.getElementById("root")
-);
-//its working
-  // <div>
-
-  //   <ul>
-  //     <li>
-  //       {add(1,2)}
-  //     </li>
-  //     <li>
-  //       {multiply(2,3)}
-  //     </li>
-  //     <li>
-  //       {subtract(3,4)}
-  //     </li>
-  //     <li>
-  //       {divide(6,6)}
-  //     </li>
-
-      
-  //   </ul>
-    
-  // </div>,
+    <App />,document.getElementById("root")
+)

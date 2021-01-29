249770c95ce4458ff0cafa780d9f4d31ee536c42
Zainmir <zainnaeemk10@gmail.com>
2021-01-29 16:54:50 +0000

    keeperapp-1 creating the basics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,13 @@
 import React from 'react';
+import Heading from './header';
+import {Note} from './Note';
+
+
+function App() {
+    return <div>
+        <Heading />
+        <Note />
+    </div>
+
+}
+export default App
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-
-function Heading(){
+const customestyle ={
+    backgroundColor:'#f5ba13',
+    width:'100%',
     
 }
+function Heading(){
+ return (
+     <div>
+     <h1 style={customestyle}>Keeper</h1>
+
+     </div>
+ )
+}
+
+export default Heading

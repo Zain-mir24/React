a19c5a0a54943f04d26842c3cce51f5db7e667db
Zainmir <zainnaeemk10@gmail.com>
2021-01-29 15:38:32 +0000

    starting keeperapp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,8 @@ import {add,multiply,subtract,divide} from "./methods"
 import List from "./list";
 
 function App() {
-    return (<div>
-
-    <ul>
-      <li>
-        {add(1,2)}
-      </li>
-      <li>
-        {multiply(2,3)}
-      </li>
-      <li>
-        {subtract(3,4)}
-      </li>
-      <li>
-        {divide(6,6)}
-      </li>
-
-      
-    </ul>
-    
-  </div>
+    return (
+    <Heading />
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App"
-
+import {add,multiply,subtract,divide} from "./components/methods"
 
 ReactDOM.render(
-  App(),
+   <App />,
   document.getElementById("root")
 );
 //its working
+  // <div>
+
+  //   <ul>
+  //     <li>
+  //       {add(1,2)}
+  //     </li>
+  //     <li>
+  //       {multiply(2,3)}
+  //     </li>
+  //     <li>
+  //       {subtract(3,4)}
+  //     </li>
+  //     <li>
+  //       {divide(6,6)}
+  //     </li>
+
+      
+  //   </ul>
+    
+  // </div>,

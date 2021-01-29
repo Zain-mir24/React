2b3b21c2b16b07386a2dd9eff79f137bfc3f4e79
Zainmir <zainnaeemk10@gmail.com>
2021-01-29 17:44:32 +0000

    keeper app challenge 1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Heading from './header';
 import {Note} from './Note';
+import {Footer} from './footer';
 
 
 function App() {
     return <div>
         <Heading />
         <Note />
+        <Footer />
     </div>
 
 }

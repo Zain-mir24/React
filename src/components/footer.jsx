ce97088f397370b70476034b42fada9f671f6f10
Zainmir <zainnaeemk10@gmail.com>
2021-02-06 18:41:04 +0000

    ecommerce project

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -20,3 +20,15 @@
 //     )
 // }
 // export {Footer}
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;

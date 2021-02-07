8008885465eac4b28bf2f75eb173b122507ebbeb
Zainmir <zainnaeemk10@gmail.com>
2021-02-07 22:29:01 +0000

    styling

---
+++ src/components/product.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+ const styling={
+ border: '1px solid black'
+ }
+const product = () => {
+    return (
+        <div>
+           <img style={styling}  src="https://tse3.mm.bing.net/th?id=OIP.OH66fh87a7_KhisDTtNd5QHaHa&pid=Api&P=0&w=300&h=300" >This image is lit</img>
+         <div>
+         <span>My mobile phone</span>
+         <p>This mobile phone is the best one out there</p> 
+         </div>
+        </div>
+    )
+}
+
+export default product
--- src/components/productlist.jsx
+++ src/components/productlist.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-
-import{Button,cart} from "react-bootstrap";
-
+import React from "react";
+import Product from "./product"
+import { Button, cart ,img} from "react-bootstrap";
+const customestyle={
+ backgroundColor:"#ded9e0",
+ 
+} 
 
 const productlist = () => {
-    return (
-        <cart className="container">
-        <cart className="row">
+  return (
+    <cart className="container" >
+      <cart className="row">
+        <cart className="col-lg-4" style={customestyle}>
+           <Product />
+        </cart>
+        <cart className="col-lg-4">
+          <Button>My Product</Button>
+        </cart>
+       
+      </cart>
+    </cart>
+  );
+};
 
-       <cart className="col-lg-6">
-                    <Button>My Product</Button>
-                 
-        </cart>
-        <cart className="col-lg-6">
-                    <Button>My Product</Button>
-                 
-        </cart>
-        </cart>
-        </cart>
-
-    )
-}
-
-export default productlist
+export default productlist;

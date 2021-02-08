7ed3070eb141d969051829342c9ac38f28fbc9b9
Zainmir <zainnaeemk10@gmail.com>
2021-02-08 16:28:41 +0000

    amazon clone

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import {Image} from "react-bootstrap"
  const styling={
  border: '1px solid black'
  }
+
 const product = () => {
     return (
         <div>
-           <img style={styling}  src="https://tse3.mm.bing.net/th?id=OIP.OH66fh87a7_KhisDTtNd5QHaHa&pid=Api&P=0&w=300&h=300" >This image is lit</img>
-         <div>
+           <Image style={styling}  src="https://tse3.mm.bing.net/th?id=OIP.OH66fh87a7_KhisDTtNd5QHaHa&pid=Api&P=0&w=300&h=300" fluid />
+       
          <span>My mobile phone</span>
-         <p>This mobile phone is the best one out there</p> 
+         <p>This mobile phone is the best one out there.Iphone will never be outta stock</p> 
          </div>
-        </div>
+        
     )
 }
 
--- src/components/productlist.jsx
+++ src/components/productlist.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import Product from "./product"
-import { Button, cart ,img} from "react-bootstrap";
+import { Button, cart,Image,Col,Row,Container } from "react-bootstrap";
 const customestyle={
- backgroundColor:"#ded9e0",
+
+ border:"1px solid black",
+ padding:"5%",
+ width:"50%"
+
  
 } 
 
 const productlist = () => {
   return (
-    <cart className="container" >
-      <cart className="row">
-        <cart className="col-lg-4" style={customestyle}>
-           <Product />
-        </cart>
-        <cart className="col-lg-4">
-          <Button>My Product</Button>
-        </cart>
+    // <cart className="container" >
+    //   <cart className="row">
+    //     <cart className="col-lg-4" style={customestyle}>
+    //        <Product />
+    //     </cart>
+    //     <cart className="col-lg-4" style={customestyle}>
+    //       <Product />
+    //     </cart>
        
-      </cart>
-    </cart>
+    //   </cart>
+    // </cart>
+    <Container>
+  <Row >
+    <Col xs={6} md={6} style={customestyle} >
+     <Product />
+    </Col>
+   
+  </Row>
+</Container>
   );
 };
 

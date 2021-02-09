7c0c7837f3e2278a63ee1ae5ee90f5b55429c109
Zainmir <zainnaeemk10@gmail.com>
2021-02-09 21:01:55 +0000

    using props

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 const custom = {
   marginTop: "100px",
-  maxWidth:"1500px",
-  marginRight:"auto",
-  marginLeft:"auto"
+  maxWidth: "1500px",
+  marginRight: "auto",
+  marginLeft: "auto",
 };
 const imgcustom = {
   width: "100%",
-  height:"500px",
+  height: "500px",
   maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
-  
 };
 const Home = () => {
   return (
@@ -17,8 +16,15 @@ const Home = () => {
       <img
         style={imgcustom}
         src="http://wgefund.org/wp-content/uploads/2016/11/AmazonSmilebanner.jpg"
-      ></img>
+      />
       {/*  */}
+      <Product
+        id={1234}
+        title="The lean hair brush"
+        price={11.96}
+        rating={5}
+        image="https://tse2.mm.bing.net/th?id=OIP.YmNbbFHsb9SPGsb51nWU9QHaGf&pid=Api&P=0&w=193&h=170"
+      />
     </div>
   );
 };

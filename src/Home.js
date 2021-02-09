df12c3326a63ad1fe28f2bcefeb0207f10887a53
Zainmir <zainnaeemk10@gmail.com>
2021-02-09 20:22:33 +0000

    working

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,25 @@
-import React from 'react'
-const custom={
-    marginTop:"100px"
-  };
+import React from "react";
+const custom = {
+  marginTop: "100px",
+  maxWidth:"1500px",
+  marginRight:"auto",
+  marginLeft:"auto"
+};
+const imgcustom = {
+  width: "100%",
+  height:"500px",
+  maskImage: "linear-gradient(to bottom, rgba(0,0,0,1), rgba(0,0,0,0))",
+  
+};
 const Home = () => {
-    return (
-        <div style={custom}>
-           <img src="https://tse3.mm.bing.net/th?id=OIP.9hRSKOHEwOTb1bN506UuygHaEC&pid=Api&P=0&w=332&h=182"></img>
-        </div>
-    )
-}
+  return (
+    <div style={custom}>
+      <img
+        style={imgcustom}
+        src="http://wgefund.org/wp-content/uploads/2016/11/AmazonSmilebanner.jpg"
+      ></img>
+    </div>
+  );
+};
 
-export default Home
+export default Home;

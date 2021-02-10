ab7b67dd7ec92c8cc7d22287b7420d9fc9f17b8f
Zainmir <zainnaeemk10@gmail.com>
2021-02-10 17:23:25 +0000

    creating datalayer

--- src/Stateprovider.js
+++ src/Stateprovider.js
@@ -1,2 +1,17 @@
 //setup data Layer
 // we need this to track the basket
+
+import React,{createContext,useContext,useReducer} from "react"
+
+//This is the DATA Layer
+
+export const Statecontext = createContext();
+
+//Build a provider
+
+export const StateProvider =({reducer,initialState,children})=>(
+   <StateContext.Provider value ={useReducer(reducer,initialState)}>
+       {children}
+   </StateContext.Provider>
+
+)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
-
+import {StateProvider} from "./StateProvider";
 
 ReactDOM.render(
-  <App />,
+  <StateProvider initialState={} reducer={}>
+    <App />
+  </StateProvider>,
   document.getElementById("root")
 );
 // var number=[1,2,3,4,6];
-
 
 //  var y =number.filter((x)=>x<=2)
 //  console.log(y)

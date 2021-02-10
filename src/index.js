c8dbc9919e3098a0261063b89a2dc87b757feeef
Zainmir <zainnaeemk10@gmail.com>
2021-02-10 22:49:04 +0000

    Reducer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import {StateProvider} from "./StateProvider";
+import reducer,{initialState} from "./Reducer"
 
 ReactDOM.render(
-  <StateProvider initialState={} reducer={}>
+  <StateProvider initialState={initialState} reducer={}>
     <App />
   </StateProvider>,
   document.getElementById("root")

b7ad3ddc61e67363f9bded7e72ec1396a85717ab
Zainmir <zainnaeemk10@gmail.com>
2021-02-12 16:06:03 +0000

    checkout page

--- src/Checkoutproduct.js
+++ src/Checkoutproduct.js
@@ -1,20 +1,29 @@
 import React from "react";
 
-const checkoutproduct = {};
-const checkoutinfo = {};
+const checkoutproduct = {
+  display: "flex",
+};
+const checkoutinfo = {
+  padding: "20px",
+};
 const productrating = {
-    display: "flex",
-  };
+  display: "flex",
+};
+const checkoutproductimage = {
+  objectFit: "contain",
+  width: "180px",
+  height: "180px",
+};
 const checkouttitle = {};
 const buttonstyle = {
-    backgroundColor: "#f0c14b",
-    borde: "1px solid",
-    borderColor: "#a88734 #9c7e31 #846a29",
-  };
+  backgroundColor: "#f0c14b",
+  borde: "1px solid",
+  borderColor: "#a88734 #9c7e31 #846a29",
+};
 const Checkoutproduct = ({ id, title, price, rating, image }) => {
   return (
     <div style={checkoutproduct}>
-      <img src={image} alt="" />
+      <img style={checkoutproductimage} src={image} alt="" />
 
       <div style={checkoutinfo}>
         <p style={checkouttitle}>{title}</p>
@@ -29,9 +38,7 @@ const Checkoutproduct = ({ id, title, price, rating, image }) => {
               <p>⭐</p>
             ))}
         </div>
-        <button style={buttonstyle}>
-          Remove from basket
-        </button>
+        <button style={buttonstyle}>Remove from basket</button>
       </div>
     </div>
   );

dab1b20ca92ae011861e9d7d7b0a48f07fc7a5f6
Zainmir <zainnaeemk10@gmail.com>
2021-02-18 21:51:20 +0000

    backend

--- backend/routes.js
+++ backend/routes.js
@@ -2,3 +2,12 @@
     const express = require('express');
     const expressValidator = require('express-validator');
     let Schema =mongoose.Schema
+    const app = express();
+    const port = process.env.PORT || 3000
+
+    app.get('/',(req,res)=>{
+        res.json('you did it')
+    })
+    app.listen(port, () => {
+        console.log('Server is up  on port' + port)
+      })
